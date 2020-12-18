2ec4f03323ff40d9e35a6e1dbad99107cb64005b
Joan Andrés <andres6936@live.com>
2020-12-18 17:32:11 -0500

    Added Test

---
+++ Test/TestBasicLog.cpp
@@ -0,0 +1,18 @@
+// Joan Andrés (@Andres6936) Github.
+
+#include "Levin/Log.hpp"
+
+using namespace Levin;
+
+int main()
+{
+	Levin::Debug() << "Test Debug String" << Levin::endl;
+
+	Levin::Info() << "Test Info String" << Levin::endl;
+
+	Levin::Warn() << "Test Warn String" << Levin::endl;
+
+	Levin::Error() << "Test Error String" << Levin::endl;
+
+	Levin::Severe() << "Test Severe String" << Levin::endl;
+}

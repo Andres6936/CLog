6536abd4b570c7e3c251692cd6c4806de2588b72
Joan Andrés <andres6936@live.com>
2020-12-18 17:47:13 -0500

    Added the File Logger

--- Test/TestBasicLog.cpp
+++ Test/TestBasicLog.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include "Levin/Log.hpp"
+#include "Levin/FileLogger.hpp"
 #include "Levin/ColoredLogger.hpp"
 
 using namespace Levin;
@@ -19,6 +20,18 @@ int main()
 	Levin::Severe() << "Test Severe String" << Levin::endl;
 
 	Levin::logger = std::make_unique<ColoredLogger>(std::wcout);
+
+	Levin::Debug() << "Test Debug String" << Levin::endl;
+
+	Levin::Info() << "Test Info String" << Levin::endl;
+
+	Levin::Warn() << "Test Warn String" << Levin::endl;
+
+	Levin::Error() << "Test Error String" << Levin::endl;
+
+	Levin::Severe() << "Test Severe String" << Levin::endl;
+
+	Levin::logger = std::make_unique<FileLogger>("Test.BasicLog.txt");
 
 	Levin::Debug() << "Test Debug String" << Levin::endl;
 

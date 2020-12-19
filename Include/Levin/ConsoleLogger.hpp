b01a9daf0a54f30e57f2f37ce2136db84c84a5c8
Joan Andrés <andres6936@live.com>
2020-12-19 14:26:47 -0500

    Added documentation

--- Include/Levin/ConsoleLogger.hpp
+++ Include/Levin/ConsoleLogger.hpp
@@ -9,6 +9,13 @@
 namespace Levin
 {
 
+	/**
+	 * In computing, a log file is a file that records either events that occur
+	 * in an operating system or other software runs,[1] or messages between
+	 * different users of a communication software. Logging is the act of
+	 * keeping a log. In the simplest case, messages are written to a single
+	 * log file.
+	 */
 	class ConsoleLogger final : public Logger
 	{
 

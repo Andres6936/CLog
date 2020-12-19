79e59d5475a5b8f670fcf91577d7cd946d8c12d8
Joan Andrés <andres6936@live.com>
2020-12-19 16:44:19 -0500

    Added reference and change the color for security levels

--- Source/ColoredLogger.cpp
+++ Source/ColoredLogger.cpp
@@ -41,7 +41,7 @@ void ColoredLogger::Message(SecurityLevel level, std::wstring_view local)
 {
 	std::lock_guard <std::mutex> guard(writeLock);
 
-	if (level == SecurityLevel::Error or level == SecurityLevel::Notice)
+	if (level >= SecurityLevel::Emergency and level <= SecurityLevel::Error)
 	{
 		// Output console in Red Darker
 		stream << "\033[1;31m" << ToString(level) << " " << GetCurrentTime() << ": " << local << "\033[0m";
@@ -56,7 +56,7 @@ void ColoredLogger::Message(SecurityLevel level, std::wstring_view local)
 		// Output console in Yellow Darker
 		stream << "\033[1;33m" << ToString(level) << " " << GetCurrentTime() << ": " << local << "\033[0m";
 	}
-	else if (level == SecurityLevel::Informational)
+	else if (level == SecurityLevel::Notice)
 	{
 		// Output console in Blue Darker
 		stream << "\033[1;34m" << ToString(level) << " " << GetCurrentTime() << ": " << local << "\033[0m";

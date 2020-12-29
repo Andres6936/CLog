8273ae3847ab29f9ef6c2bab7aab8c3c334caa0f
Joan Andrés <andres6936@live.com>
2020-12-28 21:45:42 -0500

    Added documentation

--- Include/Levin/Log.hpp
+++ Include/Levin/Log.hpp
@@ -14,48 +14,72 @@ namespace Levin
 {
 	void Log(SecurityLevel level, std::string_view message);
 
+	/**
+	 * System is unusable.
+	 */
 	template<typename ... Values>
 	inline void Emergency(const std::string& message, Values&& ... values)
 	{
 		Levin::Log(SecurityLevel::Emergency, flossy::format(message, std::forward<Values>(values)...));
 	}
 
+	/**
+	 * Action must be taken immediately.
+	 */
 	template<typename ... Values>
 	inline void Alert(const std::string& message, Values&& ... values)
 	{
 		Levin::Log(SecurityLevel::Alert, flossy::format(message, std::forward<Values>(values)...));
 	}
 
+	/**
+	 * Critical conditions.
+	 */
 	template<typename ... Values>
 	inline void Critical(const std::string& message, Values&& ... values)
 	{
 		Levin::Log(SecurityLevel::Critical, flossy::format(message, std::forward<Values>(values)...));
 	}
 
+	/**
+	 * Error conditions.
+	 */
 	template<typename ... Values>
 	inline void Error(const std::string& message, Values&& ... values)
 	{
 		Levin::Log(SecurityLevel::Error, flossy::format(message, std::forward<Values>(values)...));
 	}
 
+	/**
+	 * Warning conditions.
+	 */
 	template<typename ... Values>
 	inline void Warning(const std::string& message, Values&& ... values)
 	{
 		Levin::Log(SecurityLevel::Warning, flossy::format(message, std::forward<Values>(values)...));
 	}
 
+	/**
+	 * Normal but significant conditions.
+	 */
 	template<typename ... Values>
 	inline void Notice(const std::string& message, Values&& ... values)
 	{
 		Levin::Log(SecurityLevel::Notice, flossy::format(message, std::forward<Values>(values)...));
 	}
 
+	/**
+	 * Informational messages.
+	 */
 	template<typename ... Values>
 	inline void Informational(const std::string& message, Values&& ... values)
 	{
 		Levin::Log(SecurityLevel::Informational, flossy::format(message, std::forward<Values>(values)...));
 	}
 
+	/**
+	 * Debug-level messages.
+	 */
 	template<typename ... Values>
 	inline void Debug(const std::string& message, Values&& ... values)
 	{
